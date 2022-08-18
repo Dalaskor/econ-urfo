c432f2c4fb3d0d4e0a9a230bbfa797c627c0d981
Dalaskor <dalas.prog@gmail.com>
2022-08-18 11:21:59 +0400

    Sidebar ready

---
+++ js/SelectColor.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    var links = document.querySelectorAll(".colorinp__colorlink");
+    var buttons = document.querySelectorAll(".colorinp__cancel");
+    
+    for (let i = 0; i < links.length; i++){
+        links[i].addEventListener("click", function() {
+            if (links[i].classList.contains('colorinp__colorlink_active') === false) {
+                links[i].classList.toggle("colorinp__colorlink_active");
+                buttons[i].classList.toggle("colorinp__cancel_active");
+            }
+        });
+        buttons[i].addEventListener("click", function() {
+            if (links[i].classList.contains('colorinp__colorlink_active')) {
+                links[i].classList.toggle("colorinp__colorlink_active");
+                buttons[i].classList.toggle("colorinp__cancel_active");
+            }
+        });
+    }
+};
